b06ec86d1adcfaf5f10b1d7f45c50151ef4054fd
Jiri Ira <ira@numsolution.cz>
2021-04-18 19:42:25 +0200

    Create the menu bar

--- pypyrokin/src/gui/main_window.py
+++ pypyrokin/src/gui/main_window.py
@@ -1,15 +1,36 @@
-from PySide2.QtWidgets import QMainWindow, QSplitter
+from PySide2.QtWidgets import QMainWindow, QSplitter, QMenuBar, QAction
 from PySide2.QtCore import Qt
+from PySide2.QtGui import QKeySequence
 from pypyrokin import VERSION
 
 TITLE = f'PyPyroKin {VERSION}'
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.main_widget = None
+        self.menu_bar = None
+        self.new_menu_action = None
 
         self.setup_ui()
         self.setWindowTitle(TITLE)
 
     def setup_ui(self):
         self.main_widget = QSplitter(Qt.Horizontal)
+
+        self.create_menu_bar()
+
+    def create_menu_bar(self):
+        self.menu_bar = QMenuBar(self)
+        self.setMenuBar(self.menu_bar)
+
+        self.create_file_menu()
+
+    def create_file_menu(self):
+        file_menu = self.menu_bar.addMenu('&File')
+
+        self.new_menu_action = QAction('New', self)
+        self.new_menu_action.setShortcut(QKeySequence(Qt.CTRL + Qt.Key_N))
+
+        file_menu.addAction(self.new_menu_action)
